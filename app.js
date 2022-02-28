586c715050f9fb291d6a8d20573851a2db29fb44
AlexanderKuznetsov <alexanderanatolevic@yandex.ru>
2022-02-28 21:27:15 +0500

    feat: add userSchema & movieSchema

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const helmet = require('helmet');
+const limiter = require('./utils/limiter');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.json()); // для собирания JSON-формата
+app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+
+app.use(helmet()); // мидлвэр автоматически проставляет заголовки без-ти Content-Security-Policy
+app.use(limiter); // мидлвэр ограничения количества запросов с одного IP
+
+app.use(errors()); // обработчик ошибок celebrate
+
+app.listen(PORT);
